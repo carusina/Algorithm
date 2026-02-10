cf7ce8ebb295e8a87a93f2c9f8e888f0c8896f30
carusina <79801539+carusina@users.noreply.github.com>
2026-02-10 09:43:55 +0900

    Baekjoon - DP

---
+++ Baekjoon/11726.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+const int MAX_N = 1001;
+
+int n;
+int dp[MAX_N];
+
+int main()
+{
+	std::ios::sync_with_stdio(0);
+	std::cin.tie(0);
+
+	std::cin >> n;
+
+	dp[1] = 1;
+	dp[2] = 2;
+
+	for (int i = 3; i <= n; ++i)
+	{
+		dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
+	}
+
+	std::cout << dp[n];
+
+	return 0;
+}
