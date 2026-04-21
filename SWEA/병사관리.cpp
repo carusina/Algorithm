4ac50f417a44edd5167b97bfe53a65eca13c6193
carusina <79801539+carusina@users.noreply.github.com>
2026-04-21 17:54:58 +0900

    SWEA - DoublyLinkedList*
    Refactor soldier management functions and data structures for clarity and efficiency.

--- SWEA/병사관리.cpp
+++ SWEA/병사관리.cpp
@@ -1,164 +1,174 @@
-constexpr int MAX_SOLDIER = 100001;
-constexpr int DUMMY_START = 100000;
+// https://swexpertacademy.com/main/talk/solvingClub/problemView.do?contestProbId=AXxODdXKQAADFASZ&solveclubId=AZwrPjFqFyDHBIT3&problemBoxTitle=1%EC%A3%BC%EC%B0%A8&problemBoxCnt=6&probBoxId=AZwrPjFqFyHHBIT3
+
+// ====================
+// 상수 선언
+// ====================
+
+#define MAX_SOLDIER 100000
+#define MAX_TEAM 5
+#define MAX_SCORE 5
+#define MIN_SCORE 1
+
+// ====================
+// 구조체 선언
+// ====================
 
 struct Soldier
 {
-    int prev;
-    int next;
+	int id;
+	int team;
+
+	Soldier* prev;
+	Soldier* next;
 };
 
-Soldier soldierPool[MAX_SOLDIER + 25];
-int soldierTeam[MAX_SOLDIER];
+// ====================
+// 전역 변수
+// ====================
 
-inline int getDummy(int team, int score)
+Soldier soldierPool[MAX_SOLDIER + 1];
+Soldier heads[MAX_TEAM + 1][MAX_SCORE + 1];
+Soldier tails[MAX_TEAM + 1][MAX_SCORE + 1];
+
+// ====================
+// 유틸 함수
+// ====================
+
+inline void splice(Soldier* srcHead, Soldier* srcTail, Soldier* destTail)
 {
-    return DUMMY_START + (team - 1) * 5 + score;
+	if (srcHead->next == srcTail) return;
+
+	Soldier* srcFirst = srcHead->next;
+	Soldier* srcLast = srcTail->prev;
+
+	Soldier* destLast = destTail->prev;
+
+	destLast->next = srcFirst;
+
+	srcFirst->prev = destLast;
+	srcLast->next = destTail;
+
+	destTail->prev = srcLast;
+
+	srcHead->next = srcTail;
+	srcTail->prev = srcHead;
 }
+
+// ====================
+// API 함수
+// ====================
 
 void init()
 {
-    for (int t = 1; t <= 5; ++t)
-    {
-        for (int s = 1; s <= 5; ++s)
-        {
-            int dummy = getDummy(t, s);
-            soldierPool[dummy].prev = dummy;
-            soldierPool[dummy].next = dummy;
-        }
-    }
-}
-
-void insertNode(int mID, int team, int score)
-{
-    int dummy = getDummy(team, score);
-    int first = soldierPool[dummy].next;
-
-    soldierPool[mID].next = first;
-    soldierPool[mID].prev = dummy;
-
-    soldierPool[first].prev = mID;
-    soldierPool[dummy].next = mID;
-}
-
-void removeNode(int mID)
-{
-    int prev = soldierPool[mID].prev;
-    int next = soldierPool[mID].next;
-
-    soldierPool[prev].next = next;
-    soldierPool[next].prev = prev;
-
-    soldierPool[mID].prev = 0;
-    soldierPool[mID].next = 0;
-}
-
-void splice(int destDummy, int srcDummy)
-{
-    if (soldierPool[srcDummy].next == srcDummy)
-    {
-        return;
-    }
-
-    int srcFirst = soldierPool[srcDummy].next;
-    int srcLast = soldierPool[srcDummy].prev;
-    int destLast = soldierPool[destDummy].prev;
-
-    soldierPool[destLast].next = srcFirst;
-    soldierPool[srcFirst].prev = destLast;
-
-    soldierPool[srcLast].next = destDummy;
-    soldierPool[destDummy].prev = srcLast;
-
-    soldierPool[srcDummy].prev = srcDummy;
-    soldierPool[srcDummy].next = srcDummy;
+	for (int team = 1; team <= MAX_TEAM; ++team)
+	{
+		for(int score = MIN_SCORE; score <= MAX_SCORE; ++score)
+		{ 
+			heads[team][score].next = &tails[team][score];
+			tails[team][score].prev = &heads[team][score];
+		}
+	}
 }
 
 void hire(int mID, int mTeam, int mScore)
 {
-    soldierTeam[mID] = mTeam;
-    insertNode(mID, mTeam, mScore);
+	Soldier* soldier = &soldierPool[mID];
+	soldier->id = mID;
+	soldier->team = mTeam;
+
+	Soldier* tail = &tails[mTeam][mScore];
+	Soldier* prev = tail->prev;
+
+	prev->next = soldier;
+
+	soldier->prev = prev;
+	soldier->next = tail;
+	
+	tail->prev = soldier;
 }
 
 void fire(int mID)
 {
-    removeNode(mID);
+	Soldier* soldier = &soldierPool[mID];
+
+	Soldier* prev = soldier->prev;
+	Soldier* next = soldier->next;
+
+	prev->next = next;
+	next->prev = prev;
 }
 
 void updateSoldier(int mID, int mScore)
 {
-    removeNode(mID);
-    insertNode(mID, soldierTeam[mID], mScore);
+	Soldier* soldier = &soldierPool[mID];
+	
+	Soldier* prev = soldier->prev;
+	Soldier* next = soldier->next;
+
+	prev->next = next;
+	next->prev = prev;
+
+	Soldier* tail = &tails[soldier->team][mScore];
+	prev = tail->prev;
+
+	prev->next = soldier;
+
+	soldier->prev = prev;
+	soldier->next = tail;
+
+	tail->prev = soldier;
 }
 
 void updateTeam(int mTeam, int mChangeScore)
 {
-    if (mChangeScore == 0)
-    {
-        return;
-    }
+	if (mChangeScore > 0)
+	{
+		for (int score = MAX_SCORE - 1; score >= MIN_SCORE; --score)
+		{
+			int targetScore = (score + mChangeScore > MAX_SCORE) ? MAX_SCORE : score + mChangeScore;
 
-    if (mChangeScore > 0)
-    {
-        for (int s = 4; s >= 1; --s)
-        {
-            int destScore = s + mChangeScore;
-            if (destScore > 5)
-            {
-                destScore = 5;
-            }
+			Soldier* currHead = &heads[mTeam][score];
+			Soldier* currTail = &tails[mTeam][score];
 
-            if (s == destScore)
-            {
-                continue;
-            }
+			Soldier* targetTail = &tails[mTeam][targetScore];
 
-            splice(getDummy(mTeam, destScore), getDummy(mTeam, s));
-        }
-    }
-    else
-    {
-        for (int s = 2; s <= 5; ++s)
-        {
-            int destScore = s + mChangeScore;
-            if (destScore < 1)
-            {
-                destScore = 1;
-            }
+			splice(currHead, currTail, targetTail);
+		}
+	}
+	else if(mChangeScore < 0)
+	{
+		for (int score = MIN_SCORE + 1; score <= MAX_SCORE; ++score)
+		{
+			int targetScore = (score + mChangeScore < MIN_SCORE) ? MIN_SCORE : score + mChangeScore;
 
-            if (s == destScore)
-            {
-                continue;
-            }
+			Soldier* currHead = &heads[mTeam][score];
+			Soldier* currTail = &tails[mTeam][score];
 
-            splice(getDummy(mTeam, destScore), getDummy(mTeam, s));
-        }
-    }
+			Soldier* targetTail = &tails[mTeam][targetScore];
+
+			splice(currHead, currTail, targetTail);
+		}
+	}
 }
 
 int bestSoldier(int mTeam)
 {
-    for (int s = 5; s >= 1; --s)
-    {
-        int dummy = getDummy(mTeam, s);
-        if (soldierPool[dummy].next == dummy)
-        {
-            continue;
-        }
+	int maxSoldierId = 0;
 
-        int maxID = -1;
-        for (int curr = soldierPool[dummy].next; curr != dummy; curr = soldierPool[curr].next)
-        {
-            if (curr > maxID)
-            {
-                maxID = curr;
-            }
-        }
+	for (int score = MAX_SCORE; score >= MIN_SCORE; --score)
+	{
+		Soldier* head = &heads[mTeam][score];;
+		Soldier* tail = &tails[mTeam][score];;
+		
+		Soldier* curr = head->next;
+		while (curr != tail)
+		{
+			maxSoldierId = (maxSoldierId > curr->id) ? maxSoldierId : curr->id;
+			curr = curr->next;
+		}
 
-        if (maxID != -1)
-        {
-            return maxID;
-        }
-    }
+		if (maxSoldierId != 0) return maxSoldierId;
+	}
 
-    return -1;
+	return maxSoldierId;
 }
