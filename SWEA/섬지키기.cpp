1ea96acb71e8b30799876f5cbb04a3f559fd9a75
carusina <79801539+carusina@users.noreply.github.com>
2026-05-06 10:40:56 +0900

    SWEA - BFS***
    Refactor GetUnsubmergedArea function to simplify parameters and improve readability. Update related logic for handling submerged areas and candidate structures.

--- SWEA/섬지키기.cpp
+++ SWEA/섬지키기.cpp
@@ -7,10 +7,10 @@
 // ====================
 
 #define MAX_N 20
-#define MAX_HEIGHT 5
 #define MAX_STRUCTURE_LEN 5
 #define MAX_HASH 9999
 #define OFFSET 5
+#define MAX_DIR 4
 
 constexpr int dy[] = { -1, 1, 0, 0 };
 constexpr int dx[] = { 0, 0, -1, 1 };
@@ -48,25 +48,31 @@ bool visited[MAX_N + 2][MAX_N + 2];
 // API Helper
 // ====================
 
+inline bool IsSea(int y, int x)
+{
+	return y == 0 || y == mapSize + 1 ||
+		   x == 0 || x == mapSize + 1;
+}
+
 inline bool IsValid(int y, int x)
 {
 	return 1 <= y && y <= mapSize &&
 		   1 <= x && x <= mapSize;
 }
 
-int GetUnsubmergedArea(int mMap[MAX_N + 2][MAX_N + 2], int mSeaLevel)
+int GetUnsubmergedArea(int seaLevel)
 {
 	int submergedArea = 0;
-	
+
 	int head = 0, rear = 0;
-	for (int i = 0; i <= mapSize + 1; ++i)
+	for (int i = 0; i < mapSize + 2; ++i)
 	{
-		for (int j = 0; j <= mapSize + 1; ++j)
+		for (int j = 0; j < mapSize + 2; ++j)
 		{
-			if (i == 0 || i == mapSize + 1 || j == 0 || j == mapSize + 1)
+			if (IsSea(i, j))
 			{
-				visited[i][j] = true;
 				q[rear++] = { i, j };
+				visited[i][j] = true;
 			}
 			else visited[i][j] = false;
 		}
@@ -76,16 +82,16 @@ int GetUnsubmergedArea(int mMap[MAX_N + 2][MAX_N + 2], int mSeaLevel)
 	{
 		Pos curr = q[head++];
 
-		for (int d = 0; d < 4; ++d)
+		for (int dir = 0; dir < MAX_DIR; ++dir)
 		{
-			int ny = curr.y + dy[d];
-			int nx = curr.x + dx[d];
+			int ny = curr.y + dy[dir];
+			int nx = curr.x + dx[dir];
 
-			if (IsValid(ny, nx) && !visited[ny][nx] && mMap[ny][nx] < mSeaLevel)
+			if (IsValid(ny, nx) && !visited[ny][nx] && tempMap[ny][nx] < seaLevel)
 			{
-				++submergedArea;
-				visited[ny][nx] = true;
 				q[rear++] = { ny, nx };
+				visited[ny][nx] = true;
+				submergedArea++;
 			}
 		}
 	}
@@ -105,60 +111,63 @@ void init(int N, int mMap[20][20])
 	{
 		for (int j = 0; j < N; ++j)
 		{
-			tempMap[i + 1][j + 1] = map[i + 1][j + 1] = mMap[i][j];
+			map[i + 1][j + 1] = tempMap[i + 1][j + 1] = mMap[i][j];
 		}
 	}
 
-	for (int i = 0; i <= MAX_HASH; ++i)
+	for (int hash = 0; hash <= MAX_HASH; ++hash)
 	{
-		candidates[i].clear();
+		candidates[hash].clear();
 	}
 
-	for (int len = 2; len <= MAX_STRUCTURE_LEN; ++len)
+	// 가로
+	for (int i = 1; i <= N; ++i)
 	{
-		// 가로
-		for (int sy = 1; sy <= N; ++sy)
+		for (int len = 2; len <= MAX_STRUCTURE_LEN; ++len)
 		{
-			for (int sx = 1; sx <= N - len + 1; ++sx)
+			for (int j = 1; j <= N - len + 1; ++j)
 			{
 				int hash = 0;
-				for (int d = 1; d < len; ++d)
+				for (int d = 0; d < len - 1; ++d)
 				{
-					hash = hash * 10 + (map[sy][sx + d] - map[sy][sx + d - 1] + OFFSET);
+					hash = hash * 10 + (map[i][j + d] - map[i][j + d + 1] + OFFSET);
 				}
 
-				candidates[hash].push_back({ sy, sx, true, false });
+				candidates[hash].push_back({ i, j, true, false });
 
 				int reverseHash = 0;
-				for (int d = len - 1; d >= 1; --d)
+				for (int d = len - 2; d >= 0; --d)
 				{
-					reverseHash = reverseHash * 10 + (map[sy][sx + d - 1] - map[sy][sx + d] + OFFSET);
+					reverseHash = reverseHash * 10 + (map[i][j + d + 1] - map[i][j + d] + OFFSET);
 				}
 
-				if (reverseHash != hash) candidates[reverseHash].push_back({ sy, sx, true, true });
+				if(hash != reverseHash) candidates[reverseHash].push_back({ i, j, true, true });
 			}
 		}
+	}
 
-		// 세로
-		for (int sx = 1; sx <= N; ++sx)
+	// 세로
+	for (int j = 1; j <= N; ++j)
+	{
+		for (int len = 2; len <= MAX_STRUCTURE_LEN; ++len)
 		{
-			for (int sy = 1; sy <= N - len + 1; ++sy)
+			for (int i = 1; i <= N - len + 1; ++i)
 			{
 				int hash = 0;
-				for (int d = 1; d < len; ++d)
+				for (int d = 0; d < len - 1; ++d)
 				{
-					hash = hash * 10 + (map[sy + d][sx] - map[sy + d - 1][sx] + OFFSET);
+					hash = hash * 10 + (map[i + d][j] - map[i + d + 1][j] + OFFSET);
 				}
 
-				candidates[hash].push_back({ sy, sx, false, false });
+				candidates[hash].push_back({ i, j, false, false });
 
 				int reverseHash = 0;
-				for (int d = len - 1; d >= 1; --d)
+				for (int d = len - 2; d >= 0; --d)
 				{
-					reverseHash = reverseHash * 10 + (map[sy + d - 1][sx] - map[sy + d][sx] + OFFSET);
+					reverseHash = reverseHash * 10 + (map[i + d + 1][j] - map[i + d][j] + OFFSET);
 				}
 
-				if (reverseHash != hash) candidates[reverseHash].push_back({ sy, sx, false, true });
+				if (hash != reverseHash) candidates[reverseHash].push_back({ i, j, false, true });
 			}
 		}
 	}
@@ -169,9 +178,9 @@ int numberOfCandidate(int M, int mStructure[5])
 	if (M == 1) return mapSize * mapSize;
 
 	int hash = 0;
-	for (int d = 1; d < M; ++d)
+	for (int d = 0; d < M - 1; ++d)
 	{
-		hash = hash * 10 + (mStructure[d - 1] - mStructure[d] + OFFSET);
+		hash = hash * 10 + (mStructure[d + 1] - mStructure[d] + OFFSET);
 	}
 	
 	return (int)candidates[hash].size();
@@ -185,39 +194,41 @@ int maxArea(int M, int mStructure[5], int mSeaLevel)
 	{
 		for (int i = 1; i <= mapSize; ++i)
 		{
-			for (int j = 1; j <= mapSize; ++j)
-			{
+			for(int j = 1; j <= mapSize; ++j)
+			{ 
 				tempMap[i][j] = map[i][j] + mStructure[0];
-
-				int unsubmergedArea = GetUnsubmergedArea(tempMap, mSeaLevel);
-				area = area > unsubmergedArea ? area : unsubmergedArea;
+				
+				int unSubmergedArea = GetUnsubmergedArea(mSeaLevel);
+				area = area > unSubmergedArea ? area : unSubmergedArea;
 
 				tempMap[i][j] = map[i][j];
 			}
 		}
-
+		
 		return area;
 	}
 
 	int hash = 0;
-	for (int d = 1; d < M; ++d)
+	for (int d = 0; d < M - 1; ++d)
 	{
-		hash = hash * 10 + (mStructure[d - 1] - mStructure[d] + OFFSET);
+		hash = hash * 10 + (mStructure[d + 1] - mStructure[d] + OFFSET);
 	}
 
 	for (const Candidate& candidate : candidates[hash])
 	{
 		if (candidate.isHorizontal)
 		{
-			int height = mStructure[0] + (candidate.isReverse ? map[candidate.y][candidate.x + M - 1] : map[candidate.y][candidate.x]);
+			int height = candidate.isReverse ?
+				map[candidate.y][candidate.x + M - 1] + mStructure[0] :
+				map[candidate.y][candidate.x] + mStructure[0];
 
 			for (int d = 0; d < M; ++d)
 			{
 				tempMap[candidate.y][candidate.x + d] = height;
 			}
 
-			int unsubmergedArea = GetUnsubmergedArea(tempMap, mSeaLevel);
-			area = area > unsubmergedArea ? area : unsubmergedArea;
+			int unSubmergedArea = GetUnsubmergedArea(mSeaLevel);
+			area = area > unSubmergedArea ? area : unSubmergedArea;
 
 			for (int d = 0; d < M; ++d)
 			{
@@ -226,15 +237,17 @@ int maxArea(int M, int mStructure[5], int mSeaLevel)
 		}
 		else
 		{
-			int height = mStructure[0] + (candidate.isReverse ? map[candidate.y + M - 1][candidate.x] : map[candidate.y][candidate.x]);
+			int height = candidate.isReverse ?
+				map[candidate.y + M - 1][candidate.x] + mStructure[0] :
+				map[candidate.y][candidate.x] + mStructure[0];
 
 			for (int d = 0; d < M; ++d)
 			{
 				tempMap[candidate.y + d][candidate.x] = height;
 			}
 
-			int unsubmergedArea = GetUnsubmergedArea(tempMap, mSeaLevel);
-			area = area > unsubmergedArea ? area : unsubmergedArea;
+			int unSubmergedArea = GetUnsubmergedArea(mSeaLevel);
+			area = area > unSubmergedArea ? area : unSubmergedArea;
 
 			for (int d = 0; d < M; ++d)
 			{
@@ -242,6 +255,6 @@ int maxArea(int M, int mStructure[5], int mSeaLevel)
 			}
 		}
 	}
-
+	
 	return area;
 }
